6e6994e3fcb28b29ac16b8b7f79693ab50860e36
Daniel De Leon II <dade117@gmail.com>
2021-11-11 20:32:56 -0600

    Continuing still

--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,9 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
 	user = {'username': 'Miguel'}
-	return '''
-<html>
-	<head>
-		<title>Home Page - MicroBlog</title>
-	</head>
-	<body>
-		<h1>Hello, ''' + user['username'] + '''!</h1>
-	</body>
-</html>'''
+	return render_template('index.html', title='Home', user=user.user)
 
